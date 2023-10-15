0106c59588543108a55bf7aabb02370c0d52e72b
Kayla <kehuber@students.ntc.edu>
2023-10-15 13:44:01 -0500

    API Master wants me to pish migrations

---
+++ ApiStudyBuddy/ApiStudyBuddy/Migrations/20230929172501_Fixed Userdecks.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiStudyBuddy.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedUserdecks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserDeckGroups_DeckGroups_UserDeckGroupId",
+                table: "UserDeckGroups");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserDeckGroupId",
+                table: "UserDeckGroups",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DeckGroupId",
+                table: "DeckGroups",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DeckGroups_UserDeckGroups_DeckGroupId",
+                table: "DeckGroups",
+                column: "DeckGroupId",
+                principalTable: "UserDeckGroups",
+                principalColumn: "UserDeckGroupId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DeckGroups_UserDeckGroups_DeckGroupId",
+                table: "DeckGroups");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserDeckGroupId",
+                table: "UserDeckGroups",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DeckGroupId",
+                table: "DeckGroups",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserDeckGroups_DeckGroups_UserDeckGroupId",
+                table: "UserDeckGroups",
+                column: "UserDeckGroupId",
+                principalTable: "DeckGroups",
+                principalColumn: "DeckGroupId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
